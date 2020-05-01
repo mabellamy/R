07f7fd48182644fcd5bd4e5db56107ccfaaf3c40
Matt Bellamy <60126870+mabellamy@users.noreply.github.com>
2020-05-01 13:11:41 -0700

    Update COVID_Analysis.r

--- COVID_Analysis.r
+++ COVID_Analysis.r
@@ -1,18 +1,28 @@
 library(readr)
 library(dplyr)
+library(tidyr)
 library(ggplot2)
 
 usaData <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
-head(usaData)
+usData <- usaData %>% group_by(date, county, state)
+print(usData)
 
-viz1 <- ggplot(data=usaData, aes(x=date, y=cases)) +
-        geom_point(aes(color=cases),alpha = .75)
+stateSumCasesData <- usData %>% group_by(date, state) %>% summarize(stateCases=sum(cases))
+print(stateSumCasesData)
+
+stateSumDeathsData <- usData %>% group_by(date, state) %>% summarize(stateDeaths = sum(deaths))
+print(stateSumDeathsData)
+
+viz1 <- ggplot(data=stateSumCasesData, aes(x=date, y=stateCases)) +
+        geom_point(aes(color=state),alpha = .75) 
+        
 
 viz1
 
 
-viz2 <- ggplot(data = usaData, aes(x=date, y=deaths)) +
-        geom_point(aes(color=deaths), alpha = .65)
+viz2 <- ggplot(data = stateSumDeathsData, aes(x=date, y=stateDeaths)) +
+        geom_point(aes(color=state), alpha = .65) 
+        
         
 
 viz2
