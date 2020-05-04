b87747916674b5985863d3c0ac94f9aae4645d9c
Matt Bellamy <60126870+mabellamy@users.noreply.github.com>
2020-05-04 13:44:32 -0700

    Update COVID_Analysis.r

--- COVID_Analysis.r
+++ COVID_Analysis.r
@@ -3,39 +3,6 @@ library(dplyr)
 library(tidyr)
 library(ggplot2)
 
-usaData <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
-usData <- usaData %>% group_by(date, county, state)
-print(usData)
-
-stateSumCasesData <- usData %>% group_by(date, state) %>% summarize(stateCases = sum(cases))
-print(stateSumCasesData)
-
-stateSumDeathsData <- usData %>% group_by(date, state) %>% summarize(stateDeaths = sum(deaths))
-print(stateSumDeathsData)
-
-viz1 <- ggplot(data=stateSumCasesData, aes(x=date, y=stateCases)) +
-        geom_point(aes(color=state),alpha = .75) 
-        
-
-viz1
-
-
-viz2 <- ggplot(data = stateSumDeathsData, aes(x=date, y=stateDeaths)) +
-        geom_point(aes(color=state), alpha = .65) 
-        
-        
-
-viz2
-
-
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-library(readr)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-
 casesData <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv")
 head(casesData)
 
